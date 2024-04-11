300ceab07f96fef839b30ed5095bcb85d4ad5b5b
Mahir Ahmed <146522818+MahirWizard456@users.noreply.github.com>
2024-04-11 12:06:36 +0600

    Add files via upload

---
+++ datetime format.py
@@ -0,0 +1,33 @@
+#Time Table
+import datetime
+x=datetime.datetime.now()
+print(x)
+
+#day
+print(x.strftime("%a"))
+print(x.strftime("%A"))
+
+#Date
+print(x.strftime("%d"))
+
+#Month
+print(x.strftime("%B"))
+print(x.strftime("%b"))
+print(x.strftime("%m"))
+
+#Year
+print(x.strftime("%Y"))
+print(x.strftime("%y"))
+
+#Time
+print(x.strftime("%H"))
+print(x.strftime("%I"))
+print(x.strftime("%p"))
+print(x.strftime("%M"))
+print(x.strftime("%S"))
+
+
+
+
+
+
